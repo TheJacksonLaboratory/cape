d9ba4af4396ec170fd66f5339c4215201687e25d
Jake Emerson <Jake.Emerson@jax.org>
2019-09-26 16:28:59 -0400

    Parallel imports

--- R/singlescan.R
+++ R/singlescan.R
@@ -252,7 +252,7 @@ singlescan <- function(data.obj, geno.obj, kin.obj = NULL, n.perm = 100, alpha =
         # the following line adds package variables to the parallel worker environments
         parallel::clusterCall(cl, function(x) .libPaths(x), .libPaths())
         
-        parallel::clusterEvalQ(cl, library(cape))
+        clusterEvalQ(cl, .libPaths("/opt/cape/cape_pkg"))
         
         # Copy functions in the workspace to the workers
         # funcs <- as.vector(utils::lsf.str(envir=.GlobalEnv))
