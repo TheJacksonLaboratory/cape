a6021c483af9b5b21a2b846c890b05e9d9302851
Jake Emerson <Jake.Emerson@jax.org>
2019-10-16 08:45:52 -0400

    CI updates

--- tests/ci-tests.R
+++ tests/ci-tests.R
@@ -1,17 +1,17 @@
-#!/usr/bin/env Rscript
-args = commandArgs(trailingOnly=TRUE)
-
-library(devtools)
-library(testthat)
-library(here)
-
-cat(args[1])
-cat(" ")
-cat(print("cape" %in% (.packages())))
-
-
-# set output xml file
-options(testthat.output_file = here("test-out.xml"))
-
-# run tests
-testthat::test_dir(here("testthat"), env=.GlobalEnv, reporter = "junit")  #, filter = 'run')
+# #!/usr/bin/env Rscript
+# args = commandArgs(trailingOnly=TRUE)
+# 
+# library(devtools)
+# library(testthat)
+# library(here)
+# 
+# cat(args[1])
+# cat(" ")
+# cat(print("cape" %in% (.packages())))
+# 
+# 
+# # set output xml file
+# options(testthat.output_file = here("test-out.xml"))
+# 
+# # run tests
+# testthat::test_dir(here("testthat"), env=.GlobalEnv, reporter = "junit")
--- tests/ci-tests.sh
+++ tests/ci-tests.sh
@@ -4,5 +4,8 @@
 # Run this from the root of the package's source directory
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-echo 'Running the CAPE package tests...'
+echo 'Running the CAPE package tests in ${DIR}'
+echo 'direcotry contents...'
+ls -d */
+echo 'end directory contents.'
 Rscript --vanilla "${DIR}/ci-tests.R"
