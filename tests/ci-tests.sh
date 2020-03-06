1c410b17eee63b7306d731f70b68b7895a710ab0
elkasb <elkasb@mlg-bh0134.jax.org>
2020-03-06 14:00:51 -0500

    Try to fix test

--- tests/ci-tests.sh
+++ tests/ci-tests.sh
@@ -4,8 +4,9 @@
 # Run this from the root of the package's source directory
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-echo 'Running the CAPE package tests in ${DIR}'
-echo 'direcotry contents...'
-ls -d */
-echo 'end directory contents.'
+echo 'Running the CAPE package tests...'
+#echo 'Running the CAPE package tests in ${DIR}'
+#echo 'direcotry contents...'
+#ls -d */
+#echo 'end directory contents.'
 Rscript --vanilla "${DIR}/ci-tests.R"
