387ed2d30a50dbc655fa166ac73cc8c7d2a16343
Anna Tyler <anna.tyler@jax.org>
2020-07-24 09:36:40 -0400

    fixed get.pheno
    The labels were not matching up properly with the objects in data.obj, and if a covariate was specified it was not being corrected for.
    These are now fixed

--- R/get.pheno.R
+++ R/get.pheno.R
@@ -20,43 +20,41 @@
 #' @export
 get.pheno <- function(data.obj, scan.what = c("eigentraits", "normalized.traits", "raw.traits"), covar = NULL){
   
-  #If the user does not specify a scan_what in the data.obj,
-  #default to eigentraits, basically, if eigen,
-  #et, or ET are anywhere in the string, use the
-  #eigentraits, otherwise, use raw phenotypes
-  if (is.null(data.obj$scan_what)) {
-    data.obj$scan_what <- "eigentraits"
-  }
-  scan.what <- data.obj$scan_what
-  is.ET <- c(grep("eig", scan.what, ignore.case = TRUE), grep("ET", scan.what, ignore.case = TRUE))
-  is.raw <- grep("w", scan.what, ignore.case = TRUE)	 
-  is.norm <- grep("o", scan.what, ignore.case = TRUE)	 
+
+	scan.what <- scan.what[1] #default to eigentraits
+
+  is.ET <- as.logical(length(c(grep("eig", scan.what, ignore.case = TRUE), grep("ET", scan.what, ignore.case = TRUE))))
+  is.raw <- as.logical(length(grep("w", scan.what, ignore.case = TRUE)))
+  is.norm <- as.logical(length(grep("o", scan.what, ignore.case = TRUE)))
+
   
-  
-  if(length(is.ET) > 0){ 
+  if(is.ET){ 
     el.idx <- "ET"
-    if(length(el.idx) == 0){
+    if(is.null(data.obj$ET)){
       stop("There are no eigentraits. Run get.eigentraits() to generate eigentraits.")
     }
   }
   
-  if(length(is.raw) > 0){
-    el.idx <- "pheno"
+  if(is.raw){
+    el.idx <- "raw_pheno"
+    if(is.null(data.obj$raw_pheno)){
+    	el.idx <- "pheno"
+    }
   }
   
-  if(length(is.norm) > 0){
-    el.idx <- "raw_pheno"
-    if(length(el.idx) == 0){
-      el.idx <- "pheno"
-    }
+  if(is.norm){
+    el.idx <- "pheno"
   }
   
   pheno <- data.obj[[el.idx]]
   
   if(!is.null(covar)){
+  	covar.info <- get.covar(data.obj)
+  	covar.locale <- match(covar, covar.info$covar.names)
     models <- apply(pheno, 2, function(x) lm(x~covar.info$covar.table[,covar.locale,drop=FALSE]))
     resids <- lapply(models, residuals)
-    resid.table <- t(matrix(unlist(resids, use.names = FALSE), byrow = TRUE))
+    resid.table <- Reduce("cbind", resids)
+    colnames(resid.table) <- names(resids)
     return(resid.table)
   }
   
