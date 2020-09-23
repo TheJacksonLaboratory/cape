503ebf88b41b2c06f16ba1d67ea90b75cf3d5942
Baha El Kassaby <Baha.ElKassaby@jax.org>
2020-09-23 12:56:44 -0400

    CAPE-95 fix the load_input_and_run_cape test

--- tests/testthat/test-load-input-and-run-cape.R
+++ tests/testthat/test-load-input-and-run-cape.R
@@ -1,8 +1,10 @@
 context("test load input and run cape")
 skip_on_cran()
 
-#code for running cape with a yaml string
-#make sure there is a file called cape.parameters.txt in the results directory
+# Code for running cape with a yaml string
+# We first read the parameter file found in demo/demo_qtl and then
+# we read the file into a string which is then passed to the 
+# load_input_and_run_cape function.
 
 #===============================================================
 # load all the necessary libraries
@@ -12,82 +14,22 @@ needed.packages <- c("evd", "Matrix", "fdrtool", "shape", "corpcor", "RColorBrew
 
 for(i in 1:length(needed.packages)){library(needed.packages[i], character.only = TRUE)}
 
+# load the yaml file as a string
+demo_path <- here("demo/demo_qtl")
+param_file <- file.path(demo_path, "NON_NZO.parameters.yml")
+yaml_parameters <- readLines(param_file)
 
-yaml_parameters <- "#================================================
-# General Parameters 
-#================================================
-traits:
-  - BW_24
-  - INS_24
-  - log_GLU_24
-covariates:
-  - pgm
-scan_what:
-  - eigentraits 
-traits_scaled:
-  - true
-traits_normalized:
-  - true
-eig_which:
-  - 1
-- 2 
-pval_correction:
-  - fdr 
-use_kinship:
-  - false
-pop:
-  - 2PP
-save_results:
-  - true
-use_saved_results:
-  - false
-transform_to_phenospace:
-  - true
-
-#================================================
-# Single Scan Parameters 
-#================================================
-ref_allele:
-  - A 
-singlescan_perm:
-  - 0
-alpha:
-  - 0.05
-- 0.01
-
-#================================================
-# Marker Selection Parameters 
-#================================================
-marker_selection_method:
-  - top_effects
-peak_density:
-  - 0.5 
-tolerance:
-  - 5 
-num_alleles_in_pairscan:
-  - 84
-#================================================
-# Pairscan Parameters 
-#================================================
-max_pair_cor:
-  - 0.5 
-pairscan_null_size:
-  - 5000
-"
-
-#results_path <- file.path("results")
 results_path <- here("tests/testthat/results")
 
 # before running cape, make sure the results directory is clean
 unlink(list.files(results_path))
 data_path <- here("tests/testthat/testdata/demo_qtl_data")
 
+# we get the datafile
 data_file <- file.path(data_path, "NON_NZO_Reifsnyder_pgm_CAPE_num.csv")
 
-final_cross <- load_input_and_run_cape(input_file = data_file, yaml_params = yaml_parameters, results_path = results_path,
-                                       run_parallel = FALSE, results_file = "cross.RData", p_or_q = 0.05, 
-                                       n_cores = 4, initialize_only = FALSE, verbose = TRUE)
-
+final_cross <- load_input_and_run_cape(input_file = data_file, yaml_params = yaml_parameters, 
+                                       results_path = results_path)
 
 test_that("test that the output is not null", {
   expect_true(!is.null(final_cross))  # not null
