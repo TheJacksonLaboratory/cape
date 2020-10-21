fb1d7326c88627dacf3a8161ce3700d532fe6cd9
annatyler <anna.l.tyler@gmail.com>
2020-10-21 09:17:25 -0400

    straggling dot

--- vignettes/cape.R
+++ vignettes/cape.R
@@ -53,14 +53,15 @@ plot_pheno_cor(obesity_cross, pheno_which = c("BW_24", "INS_24", "log_GLU_24"),
 color_by = "pgm", group_labels = c("Non-obese", "Obese"))
 
 ## ----run_cape-----------------------------------------------------------------
-final_cross <- run_cape(obesity_cross, obesity_geno, results_file = "NON_NZO.RData", p_or_q = 0.05, verbose = FALSE, param_file = param_file, results_path = results_path)
+final_cross <- run_cape(obesity_cross, obesity_geno, results_file = "NON_NZO.RData", 
+p_or_q = 0.05, verbose = FALSE, param_file = param_file, results_path = results_path)
 
 ## ----eigentraits, results = "asis", echo = FALSE, out.width = 4, out.height = 4----
 svd_file <- here("demo", "demo_qtl", "svd.jpg")
 cat(paste0("![](", svd_file, ")\n"))
 
 ## ----single_plot, fig.width = 7, fig.height = 5-------------------------------
-singlescan_obj <- readRDS(here("demo", "demo_qtl", "NON_NZO.singlescan.RData"))
+singlescan_obj <- readRDS(here("demo", "demo_qtl", "NON_NZO_singlescan.RData"))
 plot_singlescan(final_cross, singlescan_obj, line_type = "h", lwd = 2, 
 covar_label_size = 1)
 
@@ -69,15 +70,15 @@ reparam_file <- here("vignettes", "reparam.png")
 cat(paste0("![](", reparam_file, ")\n"))
 
 ## ----var_inf, results = "asis", echo = FALSE----------------------------------
-var_inf_file <- here("demo", "demo_qtl", "variant.influences.jpg")
+var_inf_file <- here("demo", "demo_qtl", "variant_influences.jpg")
 cat(paste0("![](", var_inf_file, ")\n"))
 
 ## ----circ_net, results = "asis", echo = FALSE---------------------------------
-circ_net_file <- here("demo", "demo_qtl", "Network.Circular.jpg")
+circ_net_file <- here("demo", "demo_qtl", "Network_Circular.jpg")
 cat(paste0("![](", circ_net_file, ")\n"))
 
 ## ----net_vis, results = "asis", echo = FALSE----------------------------------
-net_file <- here("demo", "demo_qtl", "Network.View.jpg")
+net_file <- here("demo", "demo_qtl", "Network_View.jpg")
 cat(paste0("![](", net_file, ")\n"))
 
 ## ----plotrlang::last_error()_main, fig.width = 7, fig.height = 3--------------
